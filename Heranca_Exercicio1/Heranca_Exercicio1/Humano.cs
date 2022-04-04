aa0349b6af5efb842b5b81592a33eea3b74013dc
Karenur <68090656+Karenur@users.noreply.github.com>
2022-04-04 11:38:44 -0300

    Add files via upload

---
+++ Heranca_Exercicio1/Heranca_Exercicio1/Humano.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca_Exercicio1
+{
+    class Humano
+    {
+        private string primeiroNome;
+        private string ultimoNome;
+
+        public Humano(){}
+
+        public Humano(string primeiroNome, string ultimoNome)
+        {
+            this.primeiroNome = primeiroNome;
+            this.ultimoNome = ultimoNome;
+        }
+
+        public string PrimeiroNome
+        {
+            get
+            {
+                return primeiroNome;
+            }
+        }
+
+        public string UltimoNome
+        {
+            get
+            {
+                return ultimoNome;
+            }
+        }
+
+        public string InformarNomeCompleto()
+        {
+            return $"{PrimeiroNome} {UltimoNome}";
+        }
+    }
+}
