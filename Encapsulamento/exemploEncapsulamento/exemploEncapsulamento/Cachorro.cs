72065910b06017263d201f453b33a9089c729f0e
Karenur <68090656+Karenur@users.noreply.github.com>
2022-04-04 11:40:54 -0300

    Add files via upload

---
+++ Encapsulamento/exemploEncapsulamento/exemploEncapsulamento/Cachorro.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemploEncapsulamento
+{
+    class Cachorro
+    {
+        string nome;
+        string raca;
+        int idade;
+
+        public string Nome
+        {
+            get;set;
+        }
+
+        public string Raca
+        {
+            get
+            {
+                return raca;
+            }
+
+            set
+            {
+                raca = value;
+            }
+        }
+
+        public int Idade
+        {
+            get
+            {
+                return idade;
+            }
+
+            set
+            {
+                idade = value;
+            }
+        }
+
+        public Cachorro(string nome, string raca, int idade)
+        {
+            this.Nome = nome;
+            this.Raca = raca;
+            this.Idade = idade;
+        }
+
+
+        
+
+        
+
+    }
+}
---
+++ Poliformismo_Exercicio1/Poliformismo_Exercicio1/Livro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poliformismo_Exercicio1
+{
+    class Livro :Produto
+    {
+        private string titulo;
+        private string autor;
+
+        public Livro(string titulo, string autor, float preco):base(preco)
+        {
+            this.titulo = titulo;
+            this.autor = autor;
+        }
+
+        public string Titulo
+        {
+            get
+            {
+                return titulo;
+            }
+        }
+
+        public string Autor
+        {
+            get
+            {
+                return autor;
+            }
+        }
+        public override string InformarDescricao()
+        {
+            return $"{titulo} escrito pelo {autor}";
+        }
+
+
+    }
+}
